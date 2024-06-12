c7567c0837115be27ceadfc2ecef9a97b38c070b
Vadim Kantorov <vadimkantorov@gmail.com>
2024-06-12 17:37:19 +0200

    ...

--- log_file_access.c
+++ log_file_access.c
@@ -10,59 +10,89 @@
 #include <unistd.h>
 #include <errno.h>
 #include <dlfcn.h>
+#include <sys/stat.h>
 
 FILE* fopen(const char *path, const char *mode)
 {
-    fprintf(stderr, "log_file_access_preload: fopen(\"%s\", \"%s\")\n", path, mode);
     typedef FILE* (*orig_fopen_func_type)(const char *path, const char *mode);
+    fprintf(stderr, "log_file_access_preload: fopen(\"%s\", \"%s\")\n", path, mode);
     orig_fopen_func_type orig_func = (orig_fopen_func_type)dlsym(RTLD_NEXT, "fopen");
     return orig_func(path, mode);
 }
 
 int open(const char *path, int flags)
 {
+    typedef int (*orig_func_type)(const char *pathname, int flags);
     fprintf(stderr, "log_file_access_preload: open(\"%s\", %d)\n", path, flags);
-    typedef int (*orig_open_func_type)(const char *pathname, int flags);
-    orig_open_func_type orig_func = (orig_open_func_type)dlsym(RTLD_NEXT, "open");
+    orig_func_type orig_func = (orig_func_type)dlsym(RTLD_NEXT, "open");
     return orig_func(path, flags);
 }
-
 int open64(const char *path, int flags)
 {
+    typedef int (*orig_func_type)(const char *pathname, int flags);
     fprintf(stderr, "log_file_access_preload: open64(\"%s\", %d)\n", path, flags);
-    typedef int (*orig_open_func_type)(const char *pathname, int flags);
-    orig_open_func_type orig_func = (orig_open_func_type)dlsym(RTLD_NEXT, "open64");
+    orig_func_type orig_func = (orig_func_type)dlsym(RTLD_NEXT, "open64");
     return orig_func(path, flags);
 }
-
 //int openat(int dirfd, const char *path, int flags, mode_t mode)
 int openat(int dirfd, const char *path, int flags)
 {
+    typedef int (*orig_func_type)(int dirfd, const char *pathname, int flags);
     fprintf(stderr, "log_file_access_preload: openat(%d, \"%s\", %d)\n", dirfd, path, flags);
-    typedef int (*orig_open_func_type)(int dirfd, const char *pathname, int flags);
-    orig_open_func_type orig_func = (orig_open_func_type)dlsym(RTLD_NEXT, "openat");
+    orig_func_type orig_func = (orig_func_type)dlsym(RTLD_NEXT, "openat");
     return orig_func(dirfd, path, flags);
 }
 
 int access(const char *path, int flags)
 {
+    typedef int (*orig_func_type)(const char *pathname, int flags);
     fprintf(stderr, "log_file_access_preload: access(\"%s\", %d)\n", path, flags);
-    typedef int (*orig_open_func_type)(const char *pathname, int flags);
-    orig_open_func_type orig_func = (orig_open_func_type)dlsym(RTLD_NEXT, "access");
+    orig_func_type orig_func = (orig_func_type)dlsym(RTLD_NEXT, "access");
     return orig_func(path, flags);
 }
-
 int faccessat(int dirfd, const char *path, int mode, int flags)
 {
+    typedef int (*orig_func_type)(int dirfd, const char *pathname, int mode, int flags);
     fprintf(stderr, "log_file_access_preload: faccessat(%d, \"%s\", %d, %d)\n", dirfd, path, mode, flags);
-    typedef int (*orig_open_func_type)(int dirfd, const char *pathname, int mode, int flags);
-    orig_open_func_type orig_func = (orig_open_func_type)dlsym(RTLD_NEXT, "faccessat");
+    orig_func_type orig_func = (orig_func_type)dlsym(RTLD_NEXT, "faccessat");
     return orig_func(dirfd, path, mode, flags);
 }
 
+
+int stat(const char *restrict pathname, struct stat *restrict statbuf)
+{
+    typedef int (*orig_func_type)(const char *restrict pathname, struct stat *restrict statbuf);
+    fprintf(stderr, "log_file_access_preload: stat(\"%s\", %p)\n", pathname, (void*)statbuf);
+    orig_func_type orig_func = (orig_func_type)dlsym(RTLD_NEXT, "stat");
+    return orig_func(pathname, statbuf);
+}
+int lstat(const char *restrict pathname, struct stat *restrict statbuf)
+{
+    typedef int (*orig_func_type)(const char *restrict pathname, struct stat *restrict statbuf);
+    fprintf(stderr, "log_file_access_preload: lstat(\"%s\", %p)\n", pathname, (void*)statbuf);
+    orig_func_type orig_func = (orig_func_type)dlsym(RTLD_NEXT, "lstat");
+    return orig_func(pathname, statbuf);
+}
+int fstat(int fd, struct stat *restrict statbuf)
+{
+    typedef int (*orig_func_type)(int fd, struct stat *restrict statbuf);
+    fprintf(stderr, "log_file_access_preload: fstat(%d, %p)\n", fd, (void*)statbuf);
+    orig_func_type orig_func = (orig_func_type)dlsym(RTLD_NEXT, "fstat");
+    return orig_func(fd, statbuf);
+}
+int fstatat(int dirfd, const char *restrict pathname, struct stat *restrict statbuf, int flags)
+{
+    typedef int (*orig_func_type)(int dirfd, const char *restrict pathname, struct stat *restrict statbuf, int flags);
+    fprintf(stderr, "log_file_access_preload: fstat(%d, \"%s\", %p, %d)\n", dirfd, pathname, (void*)statbuf, flags);
+    orig_func_type orig_func = (orig_func_type)dlsym(RTLD_NEXT, "fstatat");
+    return orig_func(difd, pathname, statbuf, flags);
+}
 // 'lstat', 'fstat', 'stat', 'unlink', 'mmap', 'rename', 'link', 'rmdir', 'mkdir'
 
 #endif
+
+
+
 
 #ifdef LOGFILEACCESSSTATIC
 FILE* orig_fopen(const char *path, const char *mode);
