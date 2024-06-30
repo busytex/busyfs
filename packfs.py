52dd31fb04f05bf7ed8004803fbe5568fb6b92b5
Vadim Kantorov <vadimkantorov@gmail.com>
2024-06-30 21:32:03 +0200

    ...

--- packfs.py
+++ packfs.py
@@ -4,6 +4,7 @@ import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input-path', '-i')
+parser.add_argument('--output-path', '-o')
 args = parser.parse_args()
 
 assert os.path.exists(args.input_path) and os.path.isdir(args.input_path)
@@ -16,8 +17,14 @@ print('const char* packfs_dirs[] = {\n' + ',\n'.join('"' + repr(p)[1:-1] + '"' f
 files = []
 for (dirpath, dirnames, filenames) in os.walk(args.input_path):
     for basename in filenames:
-        files.append(os.path.join(dirpath, basename))
-print('const char* packfs_files[] = {\n' + ',\n'.join('"' + repr(p)[1:-1] + '"' for p in filenames) + '\n};\n\n')
+        if basename.endswith('.o'):
+            continue
+        p = os.path.join(dirpath, basename)
+        files.append(p)
+        subprocess.check_call(['ld', '-r', '-b', 'binary', '-o', p + '.o', p])
+        subprocess.check_call(['nm', p + '.o'])
+
+print('const char* packfs_files[] = {\n' + ',\n'.join('"' + repr(p)[1:-1] + '"' for p in files) + '\n};\n\n')
 
 # extern char _binary_updmap_pl_start[];
 # extern char _binary_updmap_pl_end[];
