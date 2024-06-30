cb5d65797a3f9c50499afd39ca7d619c885e927f
Vadim Kantorov <vadimkantorov@gmail.com>
2024-06-30 22:14:55 +0200

    ...

--- packfs.py
+++ packfs.py
@@ -9,24 +9,20 @@ args = parser.parse_args()
 
 assert os.path.exists(args.input_path) and os.path.isdir(args.input_path)
 
-dirs = []
+objects, files, dirs = [], [], []
 for (dirpath, dirnames, filenames) in os.walk(args.input_path):
     dirs.extend(os.path.join(dirpath, dirname) for dirname in dirnames)
-print('const char* packfs_dirs[] = {\n' + ',\n'.join('"' + repr(p)[1:-1] + '"' for p in dirs) + '\n};\n\n')
-
-files = []
-for (dirpath, dirnames, filenames) in os.walk(args.input_path):
     for basename in filenames:
         if basename.endswith('.o'):
             continue
         p = os.path.join(dirpath, basename)
         files.append(p)
-        subprocess.check_call(['ld', '-r', '-b', 'binary', '-o', p + '.o', p])
-        subprocess.check_call(['nm', p + '.o'])
+        objects.append(p + '.o')
+        subprocess.check_call(['ld', '-r', '-b', 'binary', '-o', objects[-1], files[-1]])
 
-print('const char* packfs_files[] = {\n' + ',\n'.join('"' + repr(p)[1:-1] + '"' for p in files) + '\n};\n\n')
+f = open(args.output_path, 'w')
+print('\n'.join('extern char _binary_{pp}_start[], _binary_{pp}_end[];' for p in objects for pp in [p.translate({ord('/') : '_', ord('.') : '_'})]), file = f)
+print('\nconst char* packfs_dirs[] = {\n' + ',\n'.join('"' + repr(p)[1:-1] + '"' for p in dirs) + '\n};\n\n', file = f)
+print('\nconst char* packfs_files[] = {\n' + ',\n'.join('"' + repr(p)[1:-1] + '"' for p in files) + '\n};\n\n', file = f)
 
-# extern char _binary_updmap_pl_start[];
-# extern char _binary_updmap_pl_end[];
-# ld -r -b binary -o updmap.o  updmap.pl
 # int iSize = (int)(_binary_updmap_pl_end - _binary_updmap_pl_start);
